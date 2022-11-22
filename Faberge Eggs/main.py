4f64923077212b98aad2b57c6b605bb2fde87a91
Gonzalo Lope Carrasco <64266133+glpcc@users.noreply.github.com>
2022-11-22 20:24:14 +0100

    tested more with no succes

--- Faberge Eggs/main.py
+++ Faberge Eggs/main.py
@@ -1,4 +1,7 @@
 from functools import cache
+import sys
+sys.setrecursionlimit(1500)
+
 
 @cache
 def height(n,m):
@@ -27,9 +30,11 @@ def test_height(n,m):
         test_height(n-1,m-1)
         test_height(n,m-1)
 
-n = 2
-m = 4
-print(height(n,m))
+n = 19477
+m = 20000
+# n = 7
+# m = 20
+# print(height(n,m))
 
 # test_list = []
 # result = 0
@@ -71,7 +76,51 @@ def test_height2(n,m):
         pascal_tree_dict = new_pascal_tree
     return result
 
-print(test_height2(n,m))
+def test_height3(n,m):
+    if n*m == 0:
+        return 0
+    if m == 1:
+        return 1
+    if n == 1:
+        return m
+    if n == 2:
+        return (m*(m+1))/2
+    if m<=n:
+        return 2**m - 1
+    pascal_tree_dict = {(n,m):1}
+    result = 1
+    for i in range(1,m-2):
+        new_pascal_tree = dict()
+        new_m = m-i
+        min_range = i-(n-2)
+        min_range = min_range*(min_range > 0)
+        max_range = i-(m-n)
+        max_range = (i+1) - max_range*(max_range > 0)
+        for j in range(min_range,max_range):
+            new_n = (n-i) + j
+            pascal_value = pascal_tree_dict.get((new_n+1,new_m+1),0) + pascal_tree_dict.get((new_n,new_m+1),0)
+            if new_n == 3:
+                result += int(pascal_value*(((new_m-1)*new_m*(new_m+1))//6 + new_m))
+            elif new_n == new_m:
+                result += int(pascal_value*int(2**(new_m)-1))
+            else:
+                result += pascal_value
+                new_pascal_tree[(new_n,new_m)] = int(pascal_value)
+        if len(new_pascal_tree) == 0:
+            return result
+        pascal_tree_dict = new_pascal_tree
+    return result
 
+from time import time
+def time_function(func,n,m):
+    a = time()
+    func(n,m)
+    b = time()
+    return b-a
+
+# print(test_height3(n,m))
+# print(time_function(test_height2,n,m))
+# print(time_function(test_height3,n,m))
+print(time_function(height,n,m))
 
 
