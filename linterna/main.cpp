c8359c244a9d82a3983ef0d40edea102ed3ca0db
Gonzalo Lope <gonzalolopecc@gmail.com>
2023-02-21 18:50:24 +0100

    t

--- linterna/main.cpp
+++ linterna/main.cpp
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-struct interval_node{
-    long long int min;
-    long long int max;
-    bool breaked;
-    interval_node *next_node;
-    bool has_next_node;
-};
+void add_element(vector<long long int> &vec,int size,long long el){
+    if (size >= vec.size()){
+        vec.push_back(el);
+    }else{
+        vec[size] = el;
+    }
+}
 
 
 int main(){
@@ -19,59 +20,94 @@ int main(){
     long long int temp;
     while (cin >> num_sobrinos)
     {
-        interval_node base_node;
-        cin >> base_node.min;
-        base_node.max = 0;
-        base_node.breaked = false;
-        base_node.has_next_node = false;
-        interval_node *selected_node = &base_node;
-        bool possible = true;
-        for (int i = 1; i < num_sobrinos; i++)
+        vector<long long int> maximos,minimos;
+        int num_intervalos = 0;
+        long long int max = 0,min = -1,new_min = -1;
+        bool posible = true;
+        for (int i = 0; i < num_sobrinos; i++)
         {
             cin >> temp;
-            // cout << "First" << selected_node -> min << " " << selected_node -> max << " " << selected_node -> breaked <<endl;
-            if (possible){
-                selected_node = &base_node;
-                while (temp < selected_node -> min && selected_node -> has_next_node)
-                {
-                    //cout << "sele " << selected_node -> min << " " << selected_node -> max << " " << selected_node -> has_next_node <<endl;
-                    selected_node = selected_node -> next_node;
-                }
-                //cout << selected_node -> min << " " << selected_node -> max << " " << selected_node -> breaked <<endl;
-                if (temp < selected_node -> min){
-                    if (!(selected_node ->breaked)){
-                        selected_node -> min = temp;
-                    }else{
-                        selected_node -> next_node = new interval_node;
-                        selected_node -> next_node -> min = temp;
-                        selected_node -> next_node -> has_next_node = false;
-                        selected_node -> next_node -> breaked = false;
-                        selected_node -> has_next_node = true;
-                    }
+            if (!posible){
+                continue;
+            }
+            // cout << "Min: " << min << " Max: " << max << " Temp: " << temp << " New Min: " << new_min;
+            // cout << " Num Intervalos: " << num_intervalos << "\n";
+            // cout << "Minimos: " << "\n";
+            // for (int k = 0; k < minimos.size(); k++)
+            // {
+            //     cout << minimos[k] << " ";
+            // }
+            // cout << "\n" ;
+            // cout << "Maximos: " << "\n";
+            // for (int k = 0; k < maximos.size(); k++)
+            // {
+            //     cout << maximos[k] << " ";
+            // }
+            // cout << "\n" ;
+            
+            if (temp < min || min == -1){
+                // cout << "Hay" << "\n";
+                if (temp < new_min || new_min == -1){
+                    new_min = temp;
                 }else{
-                    if (!(selected_node ->breaked)){
-                        //cout << "Ahora" << '\n';
-                        selected_node ->breaked = true;
-                        selected_node -> max = temp;
-                        //cout << selected_node -> max << '\n';
-                    }else{
-                        if (temp < selected_node -> max){
-                            possible = false;
-                        }else{
-                            selected_node -> max = temp;
-                        }
-                    }
+                    // Add interval if the new_min has been breaked and it is less than the actual min
+                    num_intervalos++;
+                    add_element(minimos,num_intervalos,new_min);
+                    min = new_min;
+                    add_element(maximos,num_intervalos,temp);
+                    max = temp > max ? temp : max;
+                    new_min = -1;
+                }
+                continue;
+            }
+            // If temp > min, the number of intervals cant be 0
+            // Check if it is bigger than the first one
+            if (temp > max){
+                // cout << "Hey" << "\n";
+                num_intervalos = 0;                   
+                add_element(minimos,num_intervalos,new_min == -1 ? min : new_min);
+                min = new_min == -1 ? min : new_min;
+                add_element(maximos,num_intervalos,temp);
+                num_intervalos++;
+                max = temp;
+                new_min = -1;
+                continue;
+            }
+            // Detect the first max so that the next max is <temp or 0 if there isnt more
+            int indx = 0;
+            for (int j = 0; j < num_intervalos; j++)
+            {
+                if (j == num_intervalos-1){
+                    indx = j;
+                    break;
+                }
+                if (maximos[j+1] < temp){
+                    indx = j;
+                    break;
                 }
             }
-            
+            // If the number is inside the interval
+            if (temp > minimos[indx]){
+                posible = false;
+                continue;
+            }
+            // If the maximums must be updated by a new one
+            // The indx cant be the last one because it would either be > last_min or wouldnt be here (it would be <min)
+            num_intervalos = indx + 1;
+            add_element(minimos,num_intervalos,new_min == -1 ? min : new_min);
+            min = new_min == -1 ? min : new_min;
+            add_element(maximos,num_intervalos,temp);
+            num_intervalos++;
+            new_min = -1;
+            max = temp > max ? temp : max;
         }
-        
-
-        if (possible){
-            cout << "SIEMPRE PREMIO" << '\n';
+        if (posible){
+            cout << "SIEMPRE PREMIO" << "\n";
         }else{
-            cout << "ELEGIR OTRA" << '\n';
+            cout << "ELEGIR OTRA" << "\n";
         }
+        maximos.clear();
+        minimos.clear();
     }
     
     return 0;
