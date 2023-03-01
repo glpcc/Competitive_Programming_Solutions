fd6c81e6ab08cba32ff1ff7abd71d164fbbe1e2a
Gonzalo Lope Carrasco <64266133+glpcc@users.noreply.github.com>
2023-03-01 14:01:31 +0100

    tryed construyendo dianas

--- Contruyendo_dianas/main.cpp
+++ Contruyendo_dianas/main.cpp
@@ -15,20 +15,12 @@ int num_min_darts = INF;
 
 
 int calculate_min_dart_sequence(int points,int num_sections,int num_darts){
+    //printf("Points: %d, num_sections: %d, num_darts: %d,prev_memory: %d,num_min_darts: %d, Dart_sequence:",points,num_sections,num_darts,mem[points],num_min_darts);
     if (mem[points] != -1) return mem[points];
+    if (points == 0) return 0;
     if (num_darts > num_min_darts) {
-        mem[points] = num_darts;
-        return num_darts;
-    }
-    if (points == 0){
-        if (num_darts < num_min_darts){
-            num_min_darts = num_darts;
-            for (int i = 0; i < num_darts; i++)
-            {
-                solution[i] = darts_sequence[i];
-            }
-        }
-        return num_darts;
+        mem[points] = INF;
+        return INF;
     }
     
     int result = INF;
@@ -37,12 +29,36 @@ int calculate_min_dart_sequence(int points,int num_sections,int num_darts){
     {
         if (sections[i] > points) continue;
         darts_sequence[num_darts] = sections[i];
-        temp = calculate_min_dart_sequence(points-sections[i],i,num_darts+1);
-        if (temp < result) result = temp;
+        temp = calculate_min_dart_sequence(points-sections[i],num_sections,num_darts+1);
+        if (temp < result) {
+            result = temp;
+            if (num_darts+result+1 < num_min_darts){
+                num_min_darts = num_darts+result+1;
+            }
+        }
     }
-    mem[points] = result;
-    return result;
+    //cout << points <<" " << result +1 << endl;
+    mem[points] = result + 1;
+    return result+1;
     
+}
+
+void print_series(int points, int num_sections, int min_darts){
+
+    for (int i = num_sections; i >= 0; i--)
+    {
+        if (sections[i] > points) continue;
+        if (sections[i] == points){
+            cout << " " << sections[i];
+            break;
+        }
+        if (mem[points-sections[i]] == min_darts-1){
+            cout << " " << sections[i];
+            print_series(points-sections[i],num_sections,min_darts-1);   
+            break;
+        }
+    }
+    return;
 }
 
 int main(){
@@ -58,15 +74,12 @@ int main(){
             cin >> sections[i];
         }
         calculate_min_dart_sequence(num_points,num_sections-1,0);
-        if (mem[num_points] == INF){
+        if (mem[num_points] == INF || num_min_darts == INF){
             cout << "Imposible\n";
             continue;
         }
         cout << num_min_darts << ':';
-        for (int i = 0; i < num_min_darts; i++)
-        {   
-            cout << ' ' << solution[i];
-        }
+        print_series(num_points,num_sections-1,num_min_darts);
         cout << '\n';
     }
     return 0;
